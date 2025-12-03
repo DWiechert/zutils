49368ce726d2ac87fd49c3ce503466f28081b81a
DWiechert <dan.wiechert@gmail.com>
2025-12-03 17:10:38 -0600

    Added tests for cat.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # zutils
+
 Zig re-write of uutils.
+
+## Building
+
+Build the project and all utilities with:
+```
+zig build
+```
+
+Run all tests:
+```
+zig build test --summary all
+```
+
+Run individual tests:
+```
+zig test src/cat.zig
+```
+
+Build documentation:
+```
+
+```
+
+## cat
+
+Outputs the contents of a file to stdout.
+
+Usage:
+```
+./zig-out/bin/cat <file 1> <file 2>
+```
--- src/cat.zig
+++ src/cat.zig
@@ -1,5 +1,31 @@
+//! Implementation of the `cat` command.
+//! Reads files and outputs them to std out
+
 const std = @import("std");
 
+/// Writes the contents of the provided file to the writer
+///
+/// Arguments:
+/// - `writer`: Any writer interface that supports `writeAll()`
+/// - `file_path`: Path of the file to cat
+pub fn catFile(writer: anytype, file_path: [] const u8) !void {
+    var file = try std.fs.cwd().openFile(file_path, .{});
+    defer file.close();
+
+    var buffer: [1024]u8 = undefined;
+    var file_reader = file.reader(&buffer);
+    const reader = &file_reader.interface;
+
+    while (reader.takeDelimiterInclusive('\n')) |line| {
+        try writer.writeAll(line);
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
+    }
+}
+
+/// Main entry point for the `cat` command
+/// Prints out the contents of the provided file(s) to std out
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
@@ -9,25 +35,21 @@ pub fn main() !void {
     // Skip executable
     _ = argsIterator.next();
 
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
     while (argsIterator.next()) |entry| {
-        //std.debug.print("\t\t{s}\n", .{entry});
-        var file = try std.fs.cwd().openFile(entry, .{});
-        defer file.close();
-
-        var buffer: [1024]u8 = undefined;
-        var file_reader = file.reader(&buffer);
-        const reader = &file_reader.interface;
-
-        while (reader.takeDelimiterInclusive('\n')) |line| {
-            std.debug.print("{s}", .{line});
-        } else |err| switch (err) {
-            error.EndOfStream => {},
-            else => return err,
-        }
+        try catFile(stdout, entry);
+        try stdout.flush();
     }
 
 }
 
-test "always succeeds" {
-    try std.testing.expect(true);
+test "catFile outputs test file" {
+    var output: std.ArrayList(u8) = .empty;
+    defer output.deinit(std.testing.allocator);
+
+    try catFile(output.writer(std.testing.allocator), "test_files/cat/input.txt");
+    try std.testing.expectEqualStrings("line 1\nline 2\n", output.items);
 }
