148534c102f18e746c575c16a6ebb59b8aae06e1
DWiechert <dan.wiechert@gmail.com>
2025-12-02 18:57:58 -0600

    Cat working to std out.

--- src/cat.zig
+++ src/cat.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn main() void {
+pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
     var argsIterator = try std.process.argsWithAllocator(allocator);
@@ -10,8 +10,22 @@ pub fn main() void {
     _ = argsIterator.next();
 
     while (argsIterator.next()) |entry| {
-        std.debug.print("\t\t{s}\n", .{entry});
+        //std.debug.print("\t\t{s}\n", .{entry});
+        var file = try std.fs.cwd().openFile(entry, .{});
+        defer file.close();
+
+        var buffer: [1024]u8 = undefined;
+        var file_reader = file.reader(&buffer);
+        const reader = &file_reader.interface;
+
+        while (reader.takeDelimiterInclusive('\n')) |line| {
+            std.debug.print("{s}", .{line});
+        } else |err| switch (err) {
+            error.EndOfStream => {},
+            else => return err,
+        }
     }
+
 }
 
 test "always succeeds" {
