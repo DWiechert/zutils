81c88e7c2196da787816bdf63ef5eb1ac35528c8
DWiechert <dan.wiechert@gmail.com>
2026-01-19 15:27:37 -0600

    Working on ArrayList implementation.

--- src/lib.zig
+++ src/lib.zig
@@ -4,7 +4,7 @@
 pub const ArrayList = @import("lib/arraylist.zig").ArrayList;
 
 // This runs all tests from imported files and double counts
-// tests when running `zig build test --summary all`
-// test {
-//     @import("std").testing.refAllDecls(@This());
-// }
+//tests when running `zig build test --summary all`
+test {
+    @import("std").testing.refAllDecls(@This());
+}
--- src/lib/arraylist.zig
+++ src/lib/arraylist.zig
@@ -1,36 +1,99 @@
 const std = @import("std");
 
 pub fn ArrayList(comptime T: type) type {
-    try std.debug.print("Type: {s}", .{T});
-
     return struct {
-        // Your ArrayList implementation here
         const Self = @This();
 
-        elements: []T = .{},
+        elements: []T,
+        allocator: std.mem.Allocator,
+        scale_factor: f32 = 1.5,
 
-
-        // TODO: implement
-
-        pub fn add(self: ArrayList, comptime T: type) bool {
-            return false;
+        pub fn init(allocator: std.mem.Allocator) Self {
+            // TODO: Add scale_factor as parameter
+            return Self {
+                .elements = &[_]T{},
+                .allocator = allocator,
+            };
         }
 
-        pub fn remove(self: ArrayList) type {
+        pub fn deinit(self: Self) void {
+            // `self` can be an instance since there are no modifications
+            if (self.elements.len > 0) {
+                self.allocator.free(self.elements);
+            }
+        }
+
+        pub fn add(self: *Self, element: T) bool {
+            // `self` needs to be a pointer because we are modifying the structure
+            const cur_length = self.length();
+
+            const new_elements = self.allocator.alloc(T, cur_length + 1) catch {
+                //std.debug.print("Error allocating new_elements: {s}", .{err});
+                std.debug.print("Error allocating new_elements", .{});
+                return false;
+            };
+
+            if (cur_length > 0) {
+                // Copy elements to new array to fit
+                @memcpy(new_elements[0..cur_length], self.elements);
+                self.allocator.free(self.elements);
+            }
+
+            // Add new element to add
+            new_elements[cur_length] = element;
+            self.elements = new_elements;
+
+            return true;
+        }
+
+        pub fn remove(self: *Self) T {
+            // `self` needs to be a pointer because we are modifying the structure
             return if (length() == 0) null
                 else self.elements[0];
             // TODO: Resize array
             //return error.NotImplemented;
         }
 
-        pub fn length(self: ArrayList) u8 {
+        pub fn length(self: Self) usize {
+            // `self` can be an instance since there are no modifications
             return self.elements.len;
         }
     };
 }
 
+test "empty i32" {
+    const list = ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit();
 
-test "ArrayList basic operations" {
-    // Your tests
-    try std.testing.expectEqual(true, true);
+    try std.testing.expectEqual(@as(usize, 0), list.length());
+}
+
+test "empty u8" {
+    const list = ArrayList(u8).init(std.testing.allocator);
+    defer list.deinit();
+
+    try std.testing.expectEqual(@as(usize, 0), list.length());
+}
+
+test "add i32" {
+    var list = ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit();
+
+    try std.testing.expectEqual(@as(usize, 0), list.length());
+    try std.testing.expectEqual(true, list.add(1));
+    try std.testing.expectEqual(true, list.add(2));
+    try std.testing.expectEqual(@as(usize, 2), list.length());
+}
+
+test "add u8" {
+    var list = ArrayList(u8).init(std.testing.allocator);
+    defer list.deinit();
+
+    try std.testing.expectEqual(@as(usize, 0), list.length());
+    try std.testing.expectEqual(true, list.add('h'));
+    try std.testing.expectEqual(true, list.add('e'));
+    try std.testing.expectEqual(true, list.add('l'));
+    try std.testing.expectEqual(true, list.add('l'));
+    try std.testing.expectEqual(true, list.add('o'));
+    try std.testing.expectEqual(@as(usize, 5), list.length());
 }
