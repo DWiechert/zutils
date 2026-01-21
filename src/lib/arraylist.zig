4d1c1dcc6a4d28df463cbd0d457802be8543e481
DWiechert <dan.wiechert@gmail.com>
2026-01-20 20:09:01 -0600

    Added iterator and initialization options.

--- src/lib/arraylist.zig
+++ src/lib/arraylist.zig
@@ -4,15 +4,19 @@ pub fn ArrayList(comptime T: type) type {
     return struct {
         const Self = @This();
 
+        pub const InitOptions = struct {
+            scale_factor: f32 = 1.5,
+        };
+
         elements: []T,
         allocator: std.mem.Allocator,
-        scale_factor: f32 = 1.5,
+        scale_factor: f32,
 
-        pub fn init(allocator: std.mem.Allocator) Self {
-            // TODO: Add scale_factor as parameter
+        pub fn init(allocator: std.mem.Allocator, options:InitOptions) Self {
             return Self {
                 .elements = &[_]T{},
                 .allocator = allocator,
+                .scale_factor = options.scale_factor,
             };
         }
 
@@ -23,14 +27,13 @@ pub fn ArrayList(comptime T: type) type {
             }
         }
 
-        pub fn add(self: *Self, element: T) bool {
+        pub fn add(self: *Self, element: T) !void {
             // `self` needs to be a pointer because we are modifying the structure
             const cur_length = self.length();
 
             const new_elements = self.allocator.alloc(T, cur_length + 1) catch |err| {
-                //std.debug.print("Error allocating new_elements: {s}", .{err});
                 std.debug.print("Error allocating new_elements: {}\n", .{err});
-                return false;
+                return err;
             };
 
             if (cur_length > 0) {
@@ -43,7 +46,8 @@ pub fn ArrayList(comptime T: type) type {
             new_elements[cur_length] = element;
             self.elements = new_elements;
 
-            return true;
+            // Successful, return nothing
+            return;
         }
 
         pub fn remove(self: *Self) T {
@@ -58,42 +62,90 @@ pub fn ArrayList(comptime T: type) type {
             // `self` can be an instance since there are no modifications
             return self.elements.len;
         }
+
+        pub fn iterator(self: *const Self) Iterator {
+            return Iterator {
+                .list = self,
+                .index = 0,
+            };
+        }
+
+        const Iterator = struct {
+            // This `Self` refers to the outer `ArrayList` class
+            list: *const Self,
+            index: usize,
+
+            // Cannot use `Self` here because we need to modify
+            // the `Iterator`, not the `ArrayList`
+            pub fn next(self: *Iterator) ?T {
+                const index = self.index;
+                self.index += 1;
+                return if (index < self.list.length()) self.list.elements[index]
+                else null;
+            }
+
+            pub fn reset(self: *Iterator) void {
+                self.index = 0;
+            }
+         };
     };
 }
 
 test "empty i32" {
-    const list = ArrayList(i32).init(std.testing.allocator);
+    const list = ArrayList(i32).init(std.testing.allocator, .{});
     defer list.deinit();
 
     try std.testing.expectEqual(@as(usize, 0), list.length());
 }
 
 test "empty u8" {
-    const list = ArrayList(u8).init(std.testing.allocator);
+    const list = ArrayList(u8).init(std.testing.allocator, .{});
     defer list.deinit();
 
     try std.testing.expectEqual(@as(usize, 0), list.length());
 }
 
 test "add i32" {
-    var list = ArrayList(i32).init(std.testing.allocator);
+    var list = ArrayList(i32).init(std.testing.allocator, .{});
     defer list.deinit();
 
     try std.testing.expectEqual(@as(usize, 0), list.length());
-    try std.testing.expectEqual(true, list.add(1));
-    try std.testing.expectEqual(true, list.add(2));
+    try list.add(1);
+    try list.add(2);
     try std.testing.expectEqual(@as(usize, 2), list.length());
 }
 
 test "add u8" {
-    var list = ArrayList(u8).init(std.testing.allocator);
+    var list = ArrayList(u8).init(std.testing.allocator, .{});
     defer list.deinit();
 
     try std.testing.expectEqual(@as(usize, 0), list.length());
-    try std.testing.expectEqual(true, list.add('h'));
-    try std.testing.expectEqual(true, list.add('e'));
-    try std.testing.expectEqual(true, list.add('l'));
-    try std.testing.expectEqual(true, list.add('l'));
-    try std.testing.expectEqual(true, list.add('o'));
+    try list.add('h');
+    try list.add('e');
+    try list.add('l');
+    try list.add('l');
+    try list.add('o');
     try std.testing.expectEqual(@as(usize, 5), list.length());
+}
+
+test "iterator" {
+    var list = ArrayList(i32).init(std.testing.allocator, .{});
+    defer list.deinit();
+
+    try list.add(1);
+    try list.add(2);
+
+    // Iterate once
+    var iter = list.iterator();
+    try std.testing.expectEqual(1, iter.next().?);
+    try std.testing.expectEqual(2, iter.next().?);
+    try std.testing.expectEqual(null, iter.next());
+
+    // Reset
+    iter.reset();
+
+    // Iterate again
+    try std.testing.expectEqual(1, iter.next().?);
+    try std.testing.expectEqual(2, iter.next().?);
+    try std.testing.expectEqual(null, iter.next());
 }
