cb7d1ced58456383a6b7d733b7ef7957de6c337f
Dan Wiechert <dan.wiechert@gmail.com>
2026-02-10 21:08:44 -0600

    Switch to use JSON and objects. (#13)
    * Switch to use JSON and objects.
    
    * Renamed functions.

--- src/url/shortener.zig
+++ src/url/shortener.zig
@@ -1,41 +1,41 @@
 const std = @import("std");
 
+const URLEntry = struct {
+    ttl_sec: usize,
+    url: []const u8,
+    short_code: []const u8,
+};
+
 pub const Shortener = struct {
     const Self = @This();
 
     allocator: std.mem.Allocator,
-    urls: std.StringHashMap([]const u8),
-    free_values: bool,
+    entries: std.ArrayList(URLEntry),
 
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self {
             .allocator = allocator,
-            .urls = .init(allocator),
-            .free_values = false,
+            .entries = .empty,
         };
     }
 
     pub fn deinit(self: *Self) void {
-        var iter = self.urls.keyIterator();
-        while (iter.next()) |key| {
-            // Free the duplicated short code from `add`
-            self.allocator.free(key.*);
+        // Free all strings since we duplicated them
+        // when adding to the internal list
+        for (self.entries.items) |entry| {
+            self.allocator.free(entry.url);
+            self.allocator.free(entry.short_code);
         }
 
-        if (self.free_values) {
-            var iter2 = self.urls.valueIterator();
-            while (iter2.next()) |value| {
-                self.allocator.free(value.*);
-            }
-        }
-
-        self.urls.deinit();
+        self.entries.deinit(self.allocator);
     }
 
     pub fn add(self: *Self, url: []const u8) ![]u8 {
         // Generate hash code of the URL
+        // Copy url so we manage the life-cycle independently
+        const url_copy = try self.allocator.dupe(u8, url);
         var hasher = std.hash.Wyhash.init(0);
-        hasher.update(url);
+        hasher.update(url_copy);
         const hash = hasher.final();
 
         // Convert hash code to string
@@ -45,37 +45,44 @@ pub const Shortener = struct {
         // Copy code out of buffer so no dangling pointer when function returns
         const code_copy = try self.allocator.dupe(u8, code);
 
-        std.debug.print("url: {s}\tshort_code: {s}\n", .{url, code_copy});
+        std.debug.print("url: {s}\tshort_code: {s}\n", .{url_copy, code_copy});
 
-        try self.urls.put(code_copy, url);
+        const entry: URLEntry = .{
+            .ttl_sec = 600,
+            .url = url_copy,
+            .short_code = code_copy,
+        };
+        try self.entries.append(self.allocator, entry);
+
         return code_copy;
     }
 
-    pub fn get(self: *const Self, short_code: []const u8) ?[]const u8 {
-        return self.urls.get(short_code);
-    }
-
-    pub fn findShortCode(self: *const Self, url: []const u8) ?[]const u8 {
-        var iter = self.urls.iterator();
-        while (iter.next()) |entry| {
-            if (std.mem.eql(u8, url, entry.value_ptr.*)) {
-                return entry.key_ptr.*;
+    pub fn getByShortCode(self: *const Self, short_code: []const u8) ?URLEntry {
+        for (self.entries.items) |entry| {
+            if (std.mem.eql(u8, short_code, entry.short_code)) {
+                return entry;
             }
         }
+
         return null;
     }
 
+    pub fn getByUrl(self: *const Self, url: []const u8) ?URLEntry {
+        for (self.entries.items) |entry| {
+            if (std.mem.eql(u8, url, entry.url)) {
+                return entry;
+            }
+        }
+
+        return null;
+    }
 
     pub fn write(self: *const Self, writer: anytype) !void {
         // `writer` is `anytype` as Zig does not have interfaces like Java.
         // Instead, whatever is passed as the `writer` is checked at compile
         // time if it has the correct methods or not. If yes, it compiles,
         // if not, it errors.
-
-        var iter = self.urls.iterator();
-        while (iter.next()) |entry| {
-            try writer.print("{s}={s}\n", .{entry.key_ptr.*, entry.value_ptr.*});
-        }
+        try writer.print("{f}", .{std.json.fmt(self.entries.items, .{.whitespace = .indent_1})});
     }
 
     pub fn read(allocator: std.mem.Allocator, reader: anytype) !Self {
@@ -83,23 +90,19 @@ pub const Shortener = struct {
         // Instead, whatever is passed as the `reader` is checked at compile
         // time if it has the correct methods or not. If yes, it compiles,
         // if not, it errors.
-
         var shortener = Self.init(allocator);
-        // Need to free values when reading from JSON as the values are duplicated
-        shortener.free_values = true;
 
-        var buffer: [1024]u8 = undefined;
-        while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-            var parts = std.mem.splitScalar(u8, line, '=');
-            const code = parts.next() orelse continue;
-            const url = parts.next() orelse continue;
+        // Read all content to string
+        const content = try reader.readAllAlloc(allocator, 10 * 1024 * 1024);
+        defer allocator.free(content);
 
-            // Need to duplicate the values because `lin` points to the
-            // temporary buffer which gets overwritten on each record
-            const code_copy = try allocator.dupe(u8, code);
-            const url_copy = try allocator.dupe(u8, url);
+        // Parse JSON string
+        const parsed = try std.json.parseFromSlice([]URLEntry, allocator, content, .{.allocate = .alloc_if_needed});
+        defer parsed.deinit();
 
-            try shortener.urls.put(code_copy, url_copy);
+        for (parsed.value) |item| {
+            // Discard creating short_code
+            _ = try shortener.add(item.url);
         }
 
         return shortener;
@@ -112,32 +115,38 @@ test "add" {
 
     const code = try shortener.add("https://github.com/DWiechert");
 
-    try std.testing.expectEqual(1, shortener.urls.count());
+    try std.testing.expectEqual(1, shortener.entries.items.len);
     try std.testing.expectEqualStrings("cc303afbc947d04e", code);
 }
 
-test "get" {
+test "getByShortCode" {
     var shortener = Shortener.init(std.testing.allocator);
     defer shortener.deinit();
 
     const url = "https://github.com/DWiechert";
     const code = try shortener.add(url);
 
-    try std.testing.expectEqual(1, shortener.urls.count());
-    try std.testing.expectEqualStrings(url, shortener.get(code).?);
-    try std.testing.expectEqual(null, shortener.get("asdf"));
+    try std.testing.expectEqual(1, shortener.entries.items.len);
+    const entry = shortener.getByShortCode(code).?;
+    try std.testing.expectEqualStrings(url, entry.url);
+    try std.testing.expectEqualStrings(code, entry.short_code);
+    try std.testing.expectEqual(600, entry.ttl_sec);
+    try std.testing.expectEqual(null, shortener.getByShortCode("asdf"));
 }
 
-test "findShortCode" {
+test "getByUrl" {
     var shortener = Shortener.init(std.testing.allocator);
     defer shortener.deinit();
 
     const url = "https://github.com/DWiechert";
     const code = try shortener.add(url);
 
-    try std.testing.expectEqual(1, shortener.urls.count());
-    try std.testing.expectEqualStrings(code, shortener.findShortCode(url).?);
-    try std.testing.expectEqual(null, shortener.findShortCode(code));
+    try std.testing.expectEqual(1, shortener.entries.items.len);
+    const entry = shortener.getByUrl(url).?;
+    try std.testing.expectEqualStrings(url, entry.url);
+    try std.testing.expectEqualStrings(code, entry.short_code);
+    try std.testing.expectEqual(600, entry.ttl_sec);
+    try std.testing.expectEqual(null, shortener.getByUrl(code));
 }
 
 test "write" {
@@ -152,25 +161,52 @@ test "write" {
     try shortener.write(buf.writer(std.testing.allocator));
 
     const expected =
-        \\cc303afbc947d04e=https://github.com/DWiechert
-        \\f261e152eb23b9e5=https://github.com/DWiechert/zutils
-        \\
+        \\[
+        \\ {
+        \\  "ttl_sec": 600,
+        \\  "url": "https://github.com/DWiechert",
+        \\  "short_code": "cc303afbc947d04e"
+        \\ },
+        \\ {
+        \\  "ttl_sec": 600,
+        \\  "url": "https://github.com/DWiechert/zutils",
+        \\  "short_code": "f261e152eb23b9e5"
+        \\ }
+        \\]
     ;
     try std.testing.expectEqualStrings(expected, buf.items);
 }
 
 test "read" {
     const input =
-        \\cc303afbc947d04e=https://github.com/DWiechert
-        \\f261e152eb23b9e5=https://github.com/DWiechert/zutils
-        \\
+        \\[
+        \\ {
+        \\  "url": "https://github.com/DWiechert",
+        \\  "short_code": "cc303afbc947d04e",
+        \\  "ttl_sec": 600
+        \\ },
+        \\ {
+        \\  "url": "https://github.com/DWiechert/zutils",
+        \\  "short_code": "f261e152eb23b9e5",
+        \\  "ttl_sec": 600
+        \\ }
+        \\]
     ;
     var stream = std.io.fixedBufferStream(input);
 
     var shortener = try Shortener.read(std.testing.allocator, stream.reader());
     defer shortener.deinit();
 
-    try std.testing.expectEqual(2, shortener.urls.count());
-    try std.testing.expectEqualStrings("https://github.com/DWiechert", shortener.get("cc303afbc947d04e").?);
-    try std.testing.expectEqualStrings("https://github.com/DWiechert/zutils", shortener.get("f261e152eb23b9e5").?);
+    try std.testing.expectEqual(2, shortener.entries.items.len);
+
+
+    const entry1 = shortener.getByShortCode("cc303afbc947d04e").?;
+    try std.testing.expectEqualStrings("https://github.com/DWiechert", entry1.url);
+    try std.testing.expectEqualStrings("cc303afbc947d04e", entry1.short_code);
+    try std.testing.expectEqual(600, entry1.ttl_sec);
+
+    const entry2 = shortener.getByShortCode("f261e152eb23b9e5").?;
+    try std.testing.expectEqualStrings("https://github.com/DWiechert/zutils", entry2.url);
+    try std.testing.expectEqualStrings("f261e152eb23b9e5", entry2.short_code);
+    try std.testing.expectEqual(600, entry2.ttl_sec);
 }
